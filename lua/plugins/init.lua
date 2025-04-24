26301d48e706c85b75265a3f14d112f377158efa
dknt <963725175@qq.com>
2025-04-24 20:13:31 +0800

    copilot chat

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -59,6 +59,21 @@ return {
     end,
   },
   {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken", -- Only on MacOS or Linux
+    opts = {
+      -- See Configuration section for options
+    },
+    config = function()
+      require "configs.copilot"
+    end,
+    -- See Commands section for default commands if you want to lazy load on them
+  },
+  {
     "nvim-neotest/nvim-nio",
   },
   {
